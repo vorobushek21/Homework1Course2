67f9162f7aef5df2e7a9cd1e4fdba987f605bdfc
Anton <anton.vorobev1999@mail.ru>
2023-02-12 16:43:47 +0300

    Выполнил 2 задания по наследованию

---
+++ src/transport/Bus.java
@@ -0,0 +1,9 @@
+package transport;
+
+public class Bus extends Transport{
+    public Bus(String brand, String model, String color, int year, String country, int maxSpeed) {
+        super(brand, model, color, year, country, maxSpeed);
+    }
+
+
+}
